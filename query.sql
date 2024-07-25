9e9bb0e44ff45dcb533a19e818c90e16e1217f5f
overloper <overloper0225@gmail.com>
2024-07-25 11:14:39 +0900

    frist commit

---
+++ query.sql
@@ -0,0 +1,2 @@
+CREATE TABLE KyoboBook (book_num int, title varchar(50), author varchar(50), introd varchar(50), price int, img_link varchar(100))
+CREATE TABLE AladinBook (book_num int, title varchar(50), author varchar(50), introd varchar(50), price int, img_link varchar(100))
